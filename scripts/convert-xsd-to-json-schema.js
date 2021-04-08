6b43c61e22f8a1154422d26101b43d6ef4b11a2f
Federico Carboni <fr3ddydev@gmail.com>
2021-04-08 13:27:46 +0200

    add schema

--- scripts/convert-xsd-to-json-schema.js
+++ scripts/convert-xsd-to-json-schema.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const x2j = require('jgexml/xml2json');
-const xsd = require('jgexml/xsd2json');
+const x2j = require("jgexml/xml2json");
+const xsd = require("jgexml/xsd2json");
 
-const jsonSchema = require('json-schema');
+const jsonSchema = require("json-schema");
 
-const outDir = require('./out-dir');
+const outDir = require("./out-dir");
 
-const xml = x2j.xml2json(fs.readFileSync(path.join(outDir, 'ffprobe.xsd'), 'utf8'), {
-  attributePrefix: '@',
+const xml = x2j.xml2json(fs.readFileSync(path.join(outDir, "ffprobe.xsd"), "utf8"), {
+  attributePrefix: "@",
   valueProperty: false,
   coerceTypes: false
 });
 
-const json = xsd.getJsonSchema(xml, 'ffprobe', '', false, 'xs:');
+const json = xsd.getJsonSchema(xml, "ffprobe", "", false, "xs:");
 
 json.$id = json.id;
 delete json.id;
 
 const jsonSchema1 = Object.assign({
-  title: 'ffprobe',
-  $schema: 'http://json-schema.org/schema#',
-  $id: 'ffprobe',
+  "title": "ffprobe",
+  "$schema": "http://json-schema.org/schema#",
+  "$id": "ffprobe",
 }, json.definitions.ffprobeType);
 
 delete json.definitions.ffprobeType;
@@ -32,4 +32,4 @@ jsonSchema1.definitions = json.definitions;
 
 // jsonSchema.mustBeValid(jsonSchema.validate(json));
 
-fs.writeFileSync(path.join(outDir, 'ffprobe.json'), JSON.stringify(jsonSchema1, null, 2));
+fs.writeFileSync(path.join(outDir, "ffprobe.json"), JSON.stringify(jsonSchema1, null, 2));
